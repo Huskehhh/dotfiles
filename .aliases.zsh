14e14ef16fe4d6f5d01a74d2f2eb50e6ce3d5576
Jordyn Newnham <jordyn@husk.pro>
2022-06-10 22:25:08 +1000

    Update .aliases.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -21,4 +21,6 @@ alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:2.0.0'
 alias py="python3"
 alias p="pueue"
 
-alias vim="lvim"
+alias vim="nvim"
+
+alias socks="ssh -v -D 1337 -q -C -N jordyn@host"
