82b0abfc160ef562dfa662089d473efd2467adab
Jordyn <jordyn@husk.pro>
2022-11-01 10:39:42 +1100

    swap to using github release for nvim

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 install_neovim_from_github() {
   curl -s https://api.github.com/repos/neovim/neovim/releases/latest \
-  | grep "nvim.*deb" \
+  | grep "browser_download_url.*deb" \
   | cut -d : -f 2,3 \
   | tr -d \" \
   | wget -qi -
